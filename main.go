9558d895b8333456fb82a2673ad4fc8b2ecd4e0d
AhmedReda958 <areda.gad@gmail.com>
2025-11-26 22:31:40 +0200

    Setup Task Structure and Data Management Basics

--- main.go
+++ main.go
@@ -1,5 +1,39 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
+type TaskStatus string
+
+const (
+	StatusToDo TaskStatus = "todo"
+	StatusInProgress TaskStatus = "in-progress"
+	StatusDone TaskStatus = "done"
+)
+
+type Task struct {
+	ID int `json:"id"`
+	Description string 	`json:"description"`
+	Status TaskStatus `json:"status"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
+
 func main() {
+	newTask := Task{
+		ID:1,
+		Description: "Finsh task manager project",
+		Status: StatusToDo,
+		CreatedAt: time.Now().Local(),
+		UpdatedAt: time.Now().Local(),
+	}
+
+	fmt.Println(newTask);
+
+	newTask.Status = StatusDone
+
+	fmt.Println(newTask);
 
 }
